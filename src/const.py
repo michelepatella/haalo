13bcf19b46f7a655c9dc54a43c172899420b3485
Michele Patella <patellamichele2002@gmail.com>
2026-08-01 17:52:27 +0200

    refactor: rename constants

---
+++ src/const.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+########################################################################################
+# General
+########################################################################################
+LOGO_PATH = Path("assets/images/logo.png")
+
+########################################################################################
+# Upload Page
+########################################################################################
+UPLOAD_PAGE_HEADER = "Learn faster. Explore deeper."
+UPLOAD_PAGE_SUBHEADER = "🚀 Upload an academic document to start chatting!"
+
+UPLOAD_PAGE_COLUMN_LAYOUT_NARROW = [1, 2, 1]
+UPLOAD_PAGE_COLUMN_LAYOUT_WIDE = [3, 2, 3]
+
+UPLOAD_PAGE_FILE_UPLOADER_LABEL = ""
+UPLOAD_PAGE_FILE_UPLOADER_LABEL_VISIBILITY = "hidden"
+
+UPLOAD_PAGE_UPLOADED_DOC_FORMAT = "pdf"
+
+UPLOAD_PAGE_PROCEED_BUTTON_LABEL = "Next ➔"
+
+UPLOAD_PAGE_STYLE_PATH = Path("assets/styles/upload.css")
+
+#########################################################################################
+# Session State
+#########################################################################################
+SESSION_STATE_UPLOADED_DOC_PATH_KEY = "uploaded_doc_path"
